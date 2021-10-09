5a13be833a6c5d9492ca9c8a8ba690c47fdd0bf2
someoneinjd <someoneinjd@outlook.com>
2021-10-09 09:51:45 +0800

    :wrench: Update nvim-cmp config

--- tokyonight/config/nvim/lua/plugins/cmp.lua
+++ tokyonight/config/nvim/lua/plugins/cmp.lua
@@ -13,7 +13,7 @@ end
 cmp.setup {
     snippet = {
         expand = function(args)
-            require "luasnip".lsp_expand(args.body)
+            require 'luasnip'.lsp_expand(args.body)
         end
     },
     formatting = {
@@ -22,47 +22,46 @@ cmp.setup {
             vim_item.kind = icons[vim_item.kind]
 
             vim_item.menu = ({
-                    nvim_lsp = "[LSP]",
-                    buffer = "[BUF]"
+                    nvim_lsp = '[LSP]',
+                    buffer = '[BUF]'
             })[entry.source.name]
             return vim_item
         end,
     },
     mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
         },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(t("<C-n>"), "n")
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
             elseif require 'luasnip'.expand_or_jumpable() then
-                vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
-            elseif check_back_space() then
-                vim.fn.feedkeys(t("<Tab>"), "n")
+                vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'), '')
             else
                 fallback()
             end
-        end, {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(t("<C-p>"), "n")
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
             elseif require 'luasnip'.jumpable(-1) then
-                vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+                vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
             else
                 fallback()
             end
-        end, {"i", "s"})
+        end
     },
     sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" }
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path'}
     }
 }
