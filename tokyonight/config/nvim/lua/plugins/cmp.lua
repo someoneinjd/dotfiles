9e83e237fc648334b8950a393fa9a4643433a46a
someoneinjd <someoneinjd@outlook.com>
2022-03-30 10:23:38 +0800

    Update nvim-cmp config

--- tokyonight/config/nvim/lua/plugins/cmp.lua
+++ tokyonight/config/nvim/lua/plugins/cmp.lua
@@ -1,13 +1,9 @@
 local cmp = require 'cmp'
 local icons = require 'plugins.icons'
 
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col '.' - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup {
@@ -39,29 +35,31 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
         },
-        ['<Tab>'] = function(fallback)
+        ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif require 'luasnip'.expand_or_jumpable() then
-                vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'), '')
+                require 'luasnip'.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
-        end,
-        ['<S-Tab>'] = function(fallback)
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_prev_item()
+                cmp.select_next_item()
             elseif require 'luasnip'.jumpable(-1) then
-                vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
+                require 'luasnip'.jump(-1)
             else
                 fallback()
             end
-        end
+        end, {'i', 's'})
     },
     sources = {
-        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp'},
+        { name = 'path'},
         { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path'}
+        { name = 'buffer' }
     }
 }
