2f6be508f7a48ff309e7d636195e157331068275
someoneinjd <someoneinjd@outlook.com>
2023-05-09 16:53:13 +0800

    :wrench: Fix icons

--- tokyonight/config/nvim/lua/plugins/lsp.lua
+++ tokyonight/config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 local lspconfig = require 'lspconfig';
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "󰅚 ", Warn = " ", Hint = "󰌶 ", Info = " " }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
