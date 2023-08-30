8c0cfc3c2c3ae97eeb366ef1a9627ca781ea8508
someoneinjd <someoneinjd@outlook.com>
2023-08-30 16:43:49 +0800

    :sparkles: Switch to lazy.nvim

--- tokyonight/config/nvim/init.lua
+++ tokyonight/config/nvim/init.lua
@@ -1,2 +1,17 @@
-require 'base'
-require 'pluginlist'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require "base"
+
+vim.g.mapleader = " "
+require("lazy").setup("pluginlist")
--- tokyonight/config/nvim/lua/pluginlist.lua
+++ tokyonight/config/nvim/lua/pluginlist.lua
@@ -1,132 +1,124 @@
-local filetypes = require 'base'.lsp_filetypes
+local filetypes = require "base".lsp_filetypes
 
-return require 'packer'.startup(function(use)
-    use {'wbthomason/packer.nvim'}
+return {
+    "wbthomason/packer.nvim",
 
-    use {'nvim-tree/nvim-web-devicons'}
+    "nvim-tree/nvim-web-devicons",
 
-    use {
-        'nvim-lua/plenary.nvim',
-        after = 'bufferline.nvim'
-    }
+    "nvim-lua/plenary.nvim",
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        setup = function() require 'mappings'.telescope() end,
-        after = 'plenary.nvim'
-    }
+    {
+        "nvim-telescope/telescope.nvim",
+        init = function() require "mappings".telescope() end,
+    },
 
-    use {
-        'nvim-tree/nvim-tree.lua',
-        cmd = 'NvimTreeToggle',
-        setup = function() require 'mappings'.nvimtree() end,
-        config = function() require 'nvim-tree'.setup {} end
-    }
+    {
+        "nvim-tree/nvim-tree.lua",
+        cmd = "NvimTreeToggle",
+        init = function() require "mappings".nvimtree() end,
+        config = function() require "nvim-tree".setup {} end
+    },
 
-    use {
-        'neovim/nvim-lspconfig',
-        ft = filetypes,
-        config = function() require 'plugins.lsp' end
-    }
+    {
+        "neovim/nvim-lspconfig",
+        -- ft = filetypes,
+        config = function() require "plugins.lsp" end
+    },
 
-    use {
-        'nvimdev/lspsaga.nvim',
-        after = 'nvim-lspconfig',
-        setup = function() require 'mappings'.lspsaga() end,
-        config = function() require 'lspsaga'.setup { symbol_in_winbar = { enable = false } } end
-    }
+    {
+        "nvimdev/lspsaga.nvim",
+        -- ft = filetypes,
+        init = function() require "mappings".lspsaga() end,
+        config = function() require "lspsaga".setup { symbol_in_winbar = { enable = false } } end
+    },
 
-    use {
-        'mhartington/formatter.nvim',
-        ft = filetypes,
-        cmd = 'Format',
-        setup = function() require 'mappings'.formatter() end,
-        config = function() require 'plugins.format' end
-    }
+    {
+        "mhartington/formatter.nvim",
+        -- ft = filetypes,
+        cmd = "Format",
+        init = function() require "mappings".formatter() end,
+        config = function() require "plugins.format" end
+    },
 
-    use {
-        'j-hui/fidget.nvim',
-        after = 'nvim-lspconfig',
-        config = function() require 'fidget'.setup {text = {spinner = 'dots_negative'}} end
-    }
+    {
+        "j-hui/fidget.nvim",
+        -- ft = filetypes,
+        config = function() require "fidget".setup {text = {spinner = "dots_negative"}} end
+    },
 
-    use {
-        'ray-x/lsp_signature.nvim',
-        after = 'lspsaga.nvim',
-        config = function() require 'plugins.signature' end
-    }
+    {
+        "ray-x/lsp_signature.nvim",
+        -- ft = filetypes,
+        config = function() require "plugins.signature" end
+    },
 
-    use {
-        'akinsho/bufferline.nvim',
-        setup = function() require 'mappings'.bufferline() end,
-        config = function() require 'bufferline'.setup {} end
-    }
+    {
+        "akinsho/bufferline.nvim",
+        init = function() require "mappings".bufferline() end,
+        config = function() require "bufferline".setup {} end
+    },
 
-    use {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        config = function() require 'plugins.cmp' end
-    }
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        config = function() require "plugins.cmp" end
+    },
 
-    use {
-        'L3MON4D3/LuaSnip',
-        after = 'nvim-cmp'
-    }
+    {
+        "L3MON4D3/LuaSnip",
+        -- ft = filetypes
+    },
 
-    use {
-        'saadparwaiz1/cmp_luasnip',
-        after = 'LuaSnip'
-    }
+    {
+        "saadparwaiz1/cmp_luasnip",
+        -- ft = filetypes
+    },
 
-    use {
-        'hrsh7th/cmp-nvim-lsp',
-        after = 'cmp_luasnip'
-    }
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        -- ft = filetypes
+    },
 
-    use {
-        'hrsh7th/cmp-path',
-        after = 'cmp-nvim-lsp'
-    }
+    {
+        "hrsh7th/cmp-path",
+    },
 
-    use {
-        'hrsh7th/cmp-buffer',
-        after = 'cmp-path'
-    }
+    {
+        "hrsh7th/cmp-buffer",
+    },
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        config = function() require 'plugins.eviline' end
-    }
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function() require "plugins.eviline" end
+    },
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        ft = filetypes,
-        run = ':TSUpdate',
-        event = 'BufRead',
-        config = function() require 'plugins.treesitter' end
-    }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- ft = filetypes,
+        build = ":TSUpdate",
+        config = function() require "plugins.treesitter" end
+    },
 
-    use {
-        'folke/lsp-colors.nvim',
-        after = 'nvim-lspconfig',
-        config = function() require 'lsp-colors'.setup {} end
-    }
+    {
+        "folke/lsp-colors.nvim",
+        -- ft = filetypes,
+        config = function() require "lsp-colors".setup {} end
+    },
 
-    use {
-        'windwp/nvim-autopairs',
-        after = 'nvim-cmp',
-        config = function() require 'plugins.autopairs' end
-    }
+    {
+        "windwp/nvim-autopairs",
+        config = function() require "plugins.autopairs" end
+    },
 
-    use {
-        'folke/trouble.nvim',
-        after = 'nvim-lspconfig',
-        setup = function() require 'mappings'.trouble() end,
-        config = function() require 'trouble'.setup {} end
-    }
+    {
+        "folke/trouble.nvim",
+        -- ft = filetypes,
+        init = function() require "mappings".trouble() end,
+        config = function() require "trouble".setup {} end
+    },
 
-    use {
-        'folke/tokyonight.nvim',
-        config = function() require 'plugins.theme' end
-    }
-end)
+    {
+        "folke/tokyonight.nvim",
+        config = function() require "plugins.theme" end
+    },
+}
