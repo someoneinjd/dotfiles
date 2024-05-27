75ae5bd4774a7878a3acfd8a9180da5ddbb84fe4
someoneinjd <someoneinjd@outlook.com>
2024-05-27 19:37:22 +0800

    :sparkles: Use OSC52 to copy/paste on a remote machine

--- tokyonight/config/nvim/lua/config/base.lua
+++ tokyonight/config/nvim/lua/config/base.lua
@@ -42,3 +42,25 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     end
   end,
 })
+
+-- Copy/Paste when using ssh on a remote server
+if vim.clipboard and vim.clipboard.osc52 then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup("ssh_clipboard"),
+    callback = function()
+      if vim.env.SSH_CONNECTION and vim.clipboard.osc52 then
+        vim.g.clipboard = {
+          name = "OSC 52",
+          copy = {
+            ["+"] = require("vim.clipboard.osc52").copy,
+            ["*"] = require("vim.clipboard.osc52").copy,
+          },
+          paste = {
+            ["+"] = require("vim.clipboard.osc52").paste,
+            ["*"] = require("vim.clipboard.osc52").paste,
+          },
+        }
+      end
+    end,
+  })
+end
