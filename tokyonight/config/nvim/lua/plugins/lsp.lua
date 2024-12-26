32e915363fb852a776d5f1b18ccd89e5f2dafb49
someoneinjd <someoneinjd@outlook.com>
2024-12-26 17:31:40 +0800

    :wrench: Fix lsp config

--- tokyonight/config/nvim/lua/plugins/lsp.lua
+++ tokyonight/config/nvim/lua/plugins/lsp.lua
@@ -1,121 +1,77 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      { "L3MON4D3/LuaSnip" },
-      { "saadparwaiz1/cmp_luasnip" },
-      { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-cmdline" },
-      { "onsails/lspkind.nvim" },
+    "saghen/blink.cmp",
+    build = "cargo build --release",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = {
+          "select_next",
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "snippet_backward",
+          "fallback",
+        },
+        cmdline = {
+          preset = "default",
+          ["<Tab>"] = { "select_next", "fallback" },
+          ["<S-Tab>"] = { "select_prev", "fallback" },
+        },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "buffer" },
+      },
+      signature = { enabled = true, window = { border = "rounded" } },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+           window = { border = "rounded" },
+        },
+        list = {
+          selection = function(ctx)
+            return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+          end,
+        },
+        menu = {
+          border = "rounded",
+          draw = {
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon" },
+            },
+          },
+        },
+      },
     },
-    config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_cmp()
-
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local cmp_action = lsp_zero.cmp_action()
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      cmp.setup({
-        formatting = {
-          fields = { "abbr", "kind", "menu" },
-          format = require("lspkind").cmp_format({
-            mode = "symbol", -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters
-            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-    end,
+    opts_extend = { "sources.default" },
   },
 
   {
     "neovim/nvim-lspconfig",
-    cmd = "LspInfo",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-    },
+    dependencies = { "saghen/blink.cmp" },
     config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr, exclude = { "<F4>", "<F2>", "gd", "gi", "gr" } })
-        vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = bufnr })
-      end)
-
-      lsp_zero.set_sign_icons({
-        error = "󰅚 ",
-        warn = " ",
-        hint = "󰌶 ",
-        info = " ",
-      })
-
       vim.diagnostic.config({
         virtual_text = {
           prefix = "●", -- Could be '●', '▎', 'x', ■
           spacing = 4,
+        },
+        float = { border = "rounded" },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "",
+          },
         },
       })
 
@@ -125,7 +81,18 @@ return {
             if client.server_capabilities.inlayHintProvider then
               vim.lsp.inlay_hint.enable(true)
             end
+            local opts = { buffer = bufnr }
+
+            vim.keymap.set("n", "K", "<CMD>lua vim.lsp.buf.hover()<CR>", opts)
+            vim.keymap.set("n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", opts)
+            vim.keymap.set("n", "gi", "<CMD>lua vim.lsp.buf.implementation()<CR>", opts)
+            vim.keymap.set("n", "gr", "<CMD>lua vim.lsp.buf.references()<CR>", opts)
+            vim.keymap.set("n", "ga", "<CMD>lua vim.lsp.buf.code_action()<CR>", opts)
+            vim.keymap.set("n", "[d", "<CMD>lua vim.diagnostic.goto_next()<CR>", opts)
+            vim.keymap.set("n", "]d", "<CMD>lua vim.diagnostic.goto_prev()<CR>", opts)
+            vim.keymap.set("n", "<M-r>", "<CMD>lua vim.lsp.buf.rename()<CR>", opts)
           end,
+          capabilities = require("blink.cmp").get_lsp_capabilities(),
         })
       end
     end,
@@ -167,21 +134,7 @@ return {
   {
     "j-hui/fidget.nvim",
     event = "LspAttach",
-    config = function()
-      require("fidget").setup()
-    end,
-  },
-
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "LspAttach",
-    opts = { hint_prefix = " " },
-    config = function(_, opts)
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.on_attach(function(_, bufnr)
-        require("lsp_signature").on_attach(opts, bufnr)
-      end)
-    end,
+    opts = {},
   },
 
   {
@@ -195,10 +148,8 @@ return {
     end,
     -- stylua: ignore
     keys = {
-      { "\\xx", function() require("trouble").toggle() end },
-      { "gd", function() require("trouble").open("lsp_definitions") end },
-      { "gi", function() require("trouble").open("lsp_implementations") end },
-      { "gr", function() require("trouble").open("lsp_references") end },
+      { "<localleader>xx", "<CMD>Trouble diagnostics toggle<CR>" },
+      { "<localleader>o", "<CMD>Trouble symbols toggle<CR>" }
     },
   },
 
@@ -213,16 +164,4 @@ return {
       { "<M-r>", function() return ":IncRename " .. vim.fn.expand("<cword>") end, expr = true },
     },
   },
-
-  {
-    "hedyhli/outline.nvim",
-    lazy = true,
-    cmd = { "Outline", "OutlineOpen" },
-    keys = { -- Example mapping to toggle outline
-      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
-    },
-    opts = {
-      -- Your setup opts here
-    },
-  }
 }
