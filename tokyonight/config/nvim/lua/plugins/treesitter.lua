e3bdce0cd92d18fb4b1c3ad4ea8ce15deb3d1496
someoneinjd <someoneinjd@outlook.com>
2025-01-03 14:11:07 +0800

    :sparkles: Lazy load gitsigns.nvim && nvim-treesitter

--- tokyonight/config/nvim/lua/plugins/treesitter.lua
+++ tokyonight/config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
