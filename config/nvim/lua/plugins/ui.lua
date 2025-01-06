b2a60b8837034b8a706e5c9cac55638786798fd5
someoneinjd <someoneinjd@outlook.com>
2025-01-07 00:01:27 +0800

    :wrench: Remove unused colorscheme config in neovim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -5,9 +5,6 @@ return {
     priority = 1000,
     config = function()
       vim.cmd("colorscheme tokyonight")
-      vim.g.tokyonight_style = "storm"
-      vim.g.tokyonight_italic_functions = true
-      vim.o.pumblend = 20
     end,
   },
 
