5b20e9cfd7b2ea172f94e0ddf528070dc5386b2e
someoneinjd <someoneinjd@outlook.com>
2025-01-10 21:48:23 +0800

    :sparkles: Improve telescope layout

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,17 +1,30 @@
 return {
   {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-  },
-
-  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
     },
-    config = function()
+    config = function(_, opts)
+      require("telescope").setup(opts)
       require("telescope").load_extension("fzf")
     end,
+    opts = {
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = " ",
+        entry_prefix = " ",
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+      },
+    },
     keys = {
       { "<leader>ff", "<CMD>Telescope find_files<CR>" },
       { "<leader>fg", "<CMD>Telescope live_grep<CR>" },
