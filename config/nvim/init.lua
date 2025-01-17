238d6d35ac9b2593bc599820d646bb656cad1e52
someoneinjd <someoneinjd@outlook.com>
2025-01-17 13:59:47 +0800

    :wrench: Move diagnostic config to diagnostic.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
-require("config.options")
 require("config.autocmds")
+require("config.diagnostic")
 require("config.keymaps")
 require("config.lazy")
+require("config.options")
--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>")
+vim.keymap.set("n", "]d", "<CMD>lua vim.diagnostic.goto_next()<CR>")
+vim.keymap.set("n", "[d", "<CMD>lua vim.diagnostic.goto_prev()<CR>")
