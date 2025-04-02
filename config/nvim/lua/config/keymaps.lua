f26fbafe46d576d5e868826c8b956a9bf9d82106
someoneinjd <someoneinjd@outlook.com>
2025-04-02 19:40:44 +0800

    :sparkles: Replace vim.diagnostic.goto_next / goto_prev with vim.diagnostic.jump

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>")
-vim.keymap.set("n", "]d", "<CMD>lua vim.diagnostic.goto_next()<CR>")
-vim.keymap.set("n", "[d", "<CMD>lua vim.diagnostic.goto_prev()<CR>")
+vim.keymap.set("n", "]d", "<CMD>lua vim.diagnostic.jump({count=1, float=true})<CR>")
+vim.keymap.set("n", "[d", "<CMD>lua vim.diagnostic.jump({count=-1, float=true})<CR>")
